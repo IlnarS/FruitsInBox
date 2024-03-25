f9ecc000a3eb509d932a15d471a3462c298aa222
IlnarS <146361265+IlnarS@users.noreply.github.com>
2024-03-25 23:06:12 +0500

    add files
    add files

---
+++ Program.java
@@ -0,0 +1,67 @@
+package ru.geekbrains.oop.lesson4.homework;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Program {
+
+    /**
+     Домашняя работа, задача:
+     ========================
+
+     a. Даны классы Fruit, Apple extends Fruit, Orange extends Fruit;
+     b. Класс Box, в который можно складывать фрукты. Коробки условно сортируются по типу фрукта,
+     поэтому в одну коробку нельзя сложить и яблоки, и апельсины;
+     c. Для хранения фруктов внутри коробки можно использовать ArrayList;
+     d. Сделать метод getWeight(), который высчитывает вес коробки, зная вес одного фрукта и их количество:
+     вес яблока – 1.0f, апельсина – 1.5f (единицы измерения не важны);
+     e. Внутри класса Box сделать метод compare(), который позволяет сравнить текущую коробку с той, которую
+     подадут в compare() в качестве параметра. true – если их массы равны, false в противоположном случае.
+     Можно сравнивать коробки с яблоками и апельсинами;
+     f. Написать метод, который позволяет пересыпать фрукты из текущей коробки в другую.
+     Помним про сортировку фруктов: нельзя яблоки высыпать в коробку с апельсинами.
+     Соответственно, в текущей коробке фруктов не остается, а в другую перекидываются объекты, которые были в первой;
+     g. Не забываем про метод добавления фрукта в коробку.
+     */
+    public static void main(String[] args) {
+        Integer[] intArr = new Integer[2];
+
+        intArr[0] = 1;
+        intArr[1] = 2;
+
+        System.out.println(Arrays.deepToString(intArr));
+        swap(intArr, 0, 1);
+        System.out.println(Arrays.deepToString(intArr));
+
+        Apple apple1 = new Apple();
+        Apple apple2 = new Apple();
+        Apple apple3 = new Apple();
+
+        Orange orange1 = new Orange();
+        Orange orange2 = new Orange();
+
+        Box<Apple> box1 = new Box<Apple>(apple1, apple2, apple3);
+        Box<Orange> box2 = new Box<Orange>(orange1, orange2);
+
+        System.out.println(box1.compare(box2));
+
+        Box<Orange> box3 = new Box<Orange>();
+        box2.transfer(box3);
+    }
+
+    /**
+     * Метод, который меняет два элемента массива местами.
+     */
+    public static void swap(Object[] arr, int index1, int index2) {
+        Object tmp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = tmp;
+    }
+
+    /**
+     * Mетод, который преобразует массив в ArrayList.
+     */
+    public static <T> ArrayList<T> toArrayList(T[] arr) {
+        return new ArrayList<T>(Arrays.asList(arr));
+    }
+}
